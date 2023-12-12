77ebc69d1f8303d7ef7e2a68da07bf8205957fe8
zeli <akmanzuley90@gmail.com>
2023-12-12 14:50:29 +0300

    DataBinding Lesson

--- app/src/main/java/com/example/databindingformediumarticle/MainActivity.kt
+++ app/src/main/java/com/example/databindingformediumarticle/MainActivity.kt
@@ -16,27 +16,24 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         binding.mainNesnesi = this
-        binding.sonuc.text= "0"
+        binding.hesaplamaSonucu= "0"
 
 
     }
-    fun buttonToplama(Asayi1:String,Asayi2:String){
+    fun buttonToplama(alinanSayi1:String , alinanSayi2:String){
 
-
-        val al_sayi1 = Asayi1.toInt()
-        val al_sayi2 = Asayi2.toInt()
-
+        val al_sayi1 = alinanSayi1.toInt()
+        val al_sayi2 = alinanSayi2.toInt()
         val toplam = al_sayi1+al_sayi2
 
-        binding.sonuc.text=toplam.toString()
+        binding.hesaplamaSonucu=toplam.toString()
     }
-    fun buttonCarpma(Asayi1:String,Asayi2:String){
+    fun buttonCarpma(alinanSayi1:String , alinanSayi2:String){
 
-        val sayi1 = Asayi1.toInt()
-        val sayi2 = Asayi2.toInt()
+        val al_sayi1 = alinanSayi1.toInt()
+        val al_sayi2 = alinanSayi2.toInt()
+        val carpma = al_sayi1+al_sayi2
 
-        val carp = sayi1+sayi2
-
-        binding.sonuc.text=carp.toString()
+        binding.hesaplamaSonucu=carpma.toString()
     }
 }
